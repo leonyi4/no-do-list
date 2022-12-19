721a98238d7627c51916c27107c79421fa02c9d4
Leo <114857318+leonyi4@users.noreply.github.com>
2022-12-19 21:17:28 +0700

    created form on create page

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,10 +1,40 @@
+import UploadIcon from '@mui/icons-material/Upload';
 import React from "react";
+
 
 const Create = () => {
   return (
-    <div>
-      <h1>Create </h1>
-    </div>
+    <form>
+      <label htmlFor="newToDo-Title">Enter the To Do Title</label>
+      <div clasName="newToDo-Title">
+        <input
+          type="text"
+          id="newToDo-Title"
+          // value={newToDoTitle}
+          placeholder="Enter the To Do Title"
+        />
+      </div>
+      <label htmlFor="newToDo-Desc">Enter description</label>
+      <div clasName="newToDo-Desc">
+        <input
+          type="text"
+          id="newToDo-Desc"
+          // value={newToDoDesc}
+          placeholder="Enter the To Do Description"
+        />
+      </div>
+      <div className="urgent">
+        <input type="radio" id="high" name="importance" value="HIGH"/>
+        <label for="high">High</label>
+        <input type="radio" id="middle" name="importance" value="MIDDLE"/>
+        <label for="middle">Middle</label>
+        <input type="radio" name="importance" value="LOW"/>
+        <label for="low">Low</label>
+      </div>
+      <button className="submit">
+        <UploadIcon/>
+      </button>
+    </form>
   );
 };
 
--- src/pages/View.js
+++ src/pages/View.js
@@ -1,5 +1,36 @@
 import React from "react";
-import '../styles/View.css';
+import "../styles/View.css";
+
+const dummyData = [
+  {
+    userId: 1,
+    id: 1,
+    desc: "abc",
+    title: "delectus aut autem",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 2,
+    desc: "def",
+    title: "quis ut nam facilis et officia qui",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 4,
+    desc: "hij",
+    title: "et porro tempora",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 6,
+    desc: "kmn",
+    title: "qui ullam ratione quibusdam voluptatem quia omnis",
+    completed: false,
+  },
+];
 
 const View = () => {
   return (
