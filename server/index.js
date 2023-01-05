aa116688ea8ec521493b7503bfb6be4b0703e16f
Leo <114857318+leonyi4@users.noreply.github.com>
2023-01-05 15:03:56 +0700

    added api routes

--- server/index.js
+++ server/index.js
@@ -6,12 +6,13 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json());
 
+//post a todo
 app.post("/todos", async (req, res) => {
   try {
-    const { title,description,importance } = req.body;
+    const { title, description, importance } = req.body;
     const newToDo = await pool.query(
       "INSERT INTO todo (title,description,importance) VALUES($1,$2,$3) RETURNING *",
-      [title,description,importance]
+      [title, description, importance]
     );
 
     res.json(newToDo.rows[0]);
@@ -20,9 +21,62 @@ app.post("/todos", async (req, res) => {
   }
 });
 
-pool.query("SELECT * FROM todo",(err,res)=>{
-    console.log(res.rows)
+//get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const toDos = await pool.query("SELECT * FROM todo");
+    res.json(toDos.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
 });
+
+//get a todos
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const toDo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(toDo.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+//update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updatedToDo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2 ",
+      [description, id]
+    );
+
+    res.json("To Do has been updated");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+//delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteToDO = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+
+    res.json("To Do has been deleted");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+pool
+  .query("SELECT * FROM todo ORDER BY todo_id ASC")
+  .then((res) => console.log(res.rows));
 
 app.listen(5000, () => {
   console.log("server started on port 5000");
